84d2d466398bf2462cc31607b075f4d7f3d071bf
frankmungnodev <frankmungnodev@gmail.com>
2025-04-16 01:45:57 +0530

    get user on chat list start

--- composeApp/src/commonMain/kotlin/com/jetbrains/kmpapp/screens/chatlist/ChatListViewModel.kt
+++ composeApp/src/commonMain/kotlin/com/jetbrains/kmpapp/screens/chatlist/ChatListViewModel.kt
@@ -1,10 +1,13 @@
 package com.jetbrains.kmpapp.screens.chatlist
 
 import AuthServiceClient
+import GetMeRequest
 import User
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.launch
 
 data class ChatListUiState(
     val user: User? = null
@@ -15,5 +18,18 @@ class ChatListViewModel (private val authServiceClient: AuthServiceClient) : Vie
     private val _state = MutableStateFlow(ChatListUiState())
     val state: StateFlow<ChatListUiState> = _state
 
+    init {
+        getUser()
+    }
 
+    fun getUser() {
+        viewModelScope.launch {
+            try {
+                val response = authServiceClient.GetMe().execute(GetMeRequest())
+                println("getme response => $response")
+            } catch (e: Exception) {
+                println("getme error => $e")
+            }
+        }
+    }
 }
