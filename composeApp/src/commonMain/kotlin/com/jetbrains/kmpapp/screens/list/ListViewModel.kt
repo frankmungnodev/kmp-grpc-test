ae6274e1524eea64ddff1fcf34c91d9cf103065b
frankmungnodev <frankmungnodev@gmail.com>
2025-05-09 01:18:24 +0530

    install kermit

--- composeApp/src/commonMain/kotlin/com/jetbrains/kmpapp/screens/list/ListViewModel.kt
+++ composeApp/src/commonMain/kotlin/com/jetbrains/kmpapp/screens/list/ListViewModel.kt
@@ -2,25 +2,47 @@ package com.jetbrains.kmpapp.screens.list
 
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import co.touchlab.kermit.Logger
 import com.jetbrains.kmpapp.data.MuseumObject
 import com.jetbrains.kmpapp.data.MuseumRepository
 import io.github.timortel.kmpgrpc.core.Channel
 import kotlinx.coroutines.flow.SharingStarted
 import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.collectLatest
+import kotlinx.coroutines.flow.emptyFlow
+import kotlinx.coroutines.flow.flow
 import kotlinx.coroutines.flow.stateIn
+import kotlinx.coroutines.launch
+import org.example.proto.chat.ChatMessage
 import org.example.proto.chat.ChatServiceStub
 
 class ListViewModel(museumRepository: MuseumRepository) : ViewModel() {
-    val objects: StateFlow<List<MuseumObject>> =
-        museumRepository.getObjects()
-            .stateIn(viewModelScope, SharingStarted.WhileSubscribed(5000), emptyList())
+    val objects: StateFlow<List<MuseumObject>> = museumRepository
+            .getObjects()
+            .stateIn(
+                viewModelScope,
+                SharingStarted.WhileSubscribed(5000),
+                emptyList()
+            )
 
     init {
         val channel = Channel.Builder
-            .forAddress("localhost", 50051)
+            .forAddress("127.0.0.1", 50051)
             .usePlaintext()
             .build()
 
         val stub = ChatServiceStub(channel)
+
+        viewModelScope.launch {
+            Logger.i("viewModelScope.launch")
+
+            stub.ChatStream(flow {
+                Logger.i("emit chat message")
+
+                emit(ChatMessage("ios", content = "Hello", timestamp = ""))
+            }).collectLatest { it ->
+                Logger.i("Message $it")
+            }
+        }
     }
 }
